0daf6a990331d7a2f15ee24d2e59314c5c950477
Jergus Fergus <oof.grebneke@gmail.com>
2025-01-20 12:59:45 +0100

    did very basic code

--- src/Warehouse.java
+++ src/Warehouse.java
@@ -1,5 +1,26 @@
 package src;
 
-public class Warehouse {
-    
+import sim.engine.*;
+import sim.util.*;
+import sim.field.grid.*;
+
+public class Warehouse extends SimState {
+    int height = 100;
+    int width = 100;
+    public IntGrid2D map = new IntGrid2D(width, height);
+
+    public Warehouse(long seed) {
+        super(seed);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int v = random.nextDouble() < 0.5 ? 1 : 0;
+                map.set(x, y, v);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        doLoop(Agents.class, args);
+        System.exit(0);
+    }
 }
