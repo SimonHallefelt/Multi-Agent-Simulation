bd51a8f965ed5409c6a167ab3ab4a61937baae1c
Simon_Hallefält <simon@hallefelt.se>
2025-02-03 13:40:14 +0100

    added a basic gui that displays walls

---
+++ src/Gui.java
@@ -0,0 +1,78 @@
+package src;
+
+import sim.portrayal.grid.ValueGridPortrayal2D;
+import sim.engine.*;
+import sim.display.*;
+import javax.swing.*;
+import java.awt.Color;
+import sim.util.gui.SimpleColorMap;
+
+public class Gui extends GUIState {
+    public Display2D display;
+    public JFrame displayFrame;
+    ValueGridPortrayal2D warehousePortrayal = new ValueGridPortrayal2D("typ");
+    Color[] color;
+    SimpleColorMap simpleColorMap;
+
+    public static void main(String[] args) {
+        Gui vid = new Gui();
+        Console c = new Console(vid);
+        c.setVisible(true);
+    }
+
+    public void start() {
+        super.start();
+        setupPortrayals();
+    }
+
+    public void load() {
+        super.load(state);
+        setupPortrayals();
+    }
+
+    public void setupPortrayals() {
+        Warehouse warehouse = (Warehouse) state;
+        // tell the portrayals what to portray and how to portray them
+        warehousePortrayal.setField( warehouse.map );
+        // set color map
+        color = new Color[2];
+        color[0] = Color.RED;
+        color[1] = Color.BLUE;
+        simpleColorMap = new SimpleColorMap(color);
+        warehousePortrayal.setMap(simpleColorMap);
+        // reschedule the displayer
+        display.reset();
+        display.setBackdrop(Color.white);
+        // redraw the display
+        display.repaint();
+    }
+    
+    public void init(Controller c) {
+        super.init(c);
+        display = new Display2D(600,600,this);
+        display.setClipping(false);
+        displayFrame = display.createFrame();
+        displayFrame.setTitle("Warehouse Display");
+        c.registerFrame(displayFrame); // so the frame appears in the "Display" list
+        displayFrame.setVisible(true);
+        display.attach( warehousePortrayal, "Warehouse" );
+    }
+
+    public void quit() {
+        super.quit();
+        if (displayFrame!=null) displayFrame.dispose();
+        displayFrame = null;
+        display = null;
+    }
+
+
+    public Gui() { 
+        super(new Warehouse(System.currentTimeMillis())); 
+    }
+    public Gui(SimState state) { 
+        super(state); 
+    }
+    public static String getName() { 
+        return "Student Schoolyard Cliques"; 
+    }
+}
