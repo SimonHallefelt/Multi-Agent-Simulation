c24e20fe899a4e3466262416af0d29f7b815df60
Simon_Hallefält <simon@hallefelt.se>
2025-02-03 13:35:23 +0100

    bug fix, return if move is true

--- src/Agent.java
+++ src/Agent.java
@@ -8,10 +8,10 @@ public class Agent implements Steppable {
     @Override
     public void step(SimState state) {
         Warehouse warehouse = (Warehouse) state;
-        if (!warehouse.move(this, 1, 0)) return;
-        if (!warehouse.move(this, 0, 1)) return;
-        if (!warehouse.move(this, -1, 0)) return;
-        if (!warehouse.move(this, 0, -1)) return;
+        if (warehouse.move(this, 1, 0)) return;
+        if (warehouse.move(this, 0, 1)) return;
+        if (warehouse.move(this, -1, 0)) return;
+        if (warehouse.move(this, 0, -1)) return;
     }
     
 }
