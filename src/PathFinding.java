25b08b74eae3534bf512c251f3fc50131040b5fa
Simon_Hallefält <simon@hallefelt.se>
2025-02-10 14:44:48 +0100

    added pacman pathfinding

--- src/PathFinding.java
+++ src/PathFinding.java
@@ -5,7 +5,7 @@ public class PathFinding {
     int dy = 0;
 
 
-    public void pickDirection(Warehouse warehouse) {
+    public void randomWalk(Warehouse warehouse) {
         double d = warehouse.random.nextDouble();
         boolean vert = (dx == 0);
         if (d < 0.25) {
@@ -15,6 +15,38 @@ public class PathFinding {
         else if (d < 0.5) {
             if (vert) { dx = -1; dy = 0; }
             else      { dx = 0; dy = -1; }
+        }
+    }
+
+    public void pacman(Warehouse warehouse, int targetx, int targety, int x, int y) {
+        int disx = Math.abs(targetx-x);
+        int disy = Math.abs(targety-y);
+        int tempx = dx;
+        if ((!warehouse.isOccupied(x+dx, y+dy)) && 
+        ((Math.abs(targetx-(x+dx)) < disx) || 
+        (Math.abs(targety-(y+dy)) < disy))) {
+            return;
+        } else if ((!warehouse.isOccupied(x+dy, y+dx)) && 
+        ((Math.abs(targetx-(x+dy)) < disx) || 
+        (Math.abs(targety-(y+dx)) < disy))) {
+            dx = dy;
+            dy = tempx;
+        } else if ((!warehouse.isOccupied(x-dy, y-dx)) && 
+        ((Math.abs(targetx-(x-dy)) < disx) || 
+        (Math.abs(targety-(y-dx)) < disy))) {
+            dx = -dy;
+            dy = -tempx;
+        } else if (!warehouse.isOccupied(x+dx, y+dy)) {
+            return;
+        } else if (!warehouse.isOccupied(x+dy, y+dx)) {
+            dx = dy;
+            dy = tempx;
+        } else if (!warehouse.isOccupied(x-dy, y-dx)) {
+            dx = -dy;
+            dy = -tempx;
+        } else {
+            dx = -dx;
+            dy = -dy;
         }
     }
 
