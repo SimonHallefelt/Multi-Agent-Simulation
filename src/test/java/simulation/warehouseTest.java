43cb38d3b6ee8f8ad08dc3ea1a48f01f9d0a8466
Simon_Hallefält <simon@hallefelt.se>
2025-03-27 13:04:39 +0100

    removed test, that used random files

--- src/test/java/simulation/warehouseTest.java
+++ src/test/java/simulation/warehouseTest.java
@@ -12,50 +12,6 @@ import org.junit.jupiter.api.Test;
 public class warehouseTest {
 
     @Test void test_1_warehouse() {
-        int status = -1;
-        String[] args = {"-until", "1000"};
-        Exception[] threadException = {null};
-        
-        try {
-            // Custom SecurityManager
-            System.setSecurityManager(new SecurityManager() {
-                @Override
-                public void checkPermission(java.security.Permission perm) {}
-
-                @Override
-                public void checkExit(int status) {
-                    throw new ExitException(status);
-                }
-            });
-
-            // threadException captures exceptions from threads
-            Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
-                if (throwable instanceof Exception) {
-                    threadException[0] = (Exception) throwable;
-                }
-            });
-
-            // Function invokes "System.exit(0);"
-            Warehouse.doLoop(Warehouse.class, args); 
-            fail("Expected System.exit");
-        } catch (ExitException e) {
-            status = e.getStatus();
-        } catch (Exception e){
-            fail("Exception in SimState " + e);
-        }
-
-        if (threadException[0] != null) {
-            Exception e = threadException[0];
-            StringWriter sw = new StringWriter();
-            PrintWriter pw = new PrintWriter(sw);
-            e.printStackTrace(pw);
-            System.out.println("Exception during test: " + sw.toString());
-            fail("Exception during test: " + sw.toString());
-        }
-        assertEquals(0, status);
-    }
-
-    @Test void test_2_warehouse() {
         Warehouse state = new Warehouse(0, "src\\test\\resources\\simple\\warehouse_3.json");
         state.start();
         do
@@ -64,18 +20,5 @@ public class warehouseTest {
         state.finish();
 
         assertTrue(true);
-    }
-
-    // Custom exception for exit
-    private static class ExitException extends SecurityException {
-        private int status;
-
-        public ExitException(int status) {
-            this.status = status;
-        }
-
-        public int getStatus() {
-            return status;
-        }
     }
 }
